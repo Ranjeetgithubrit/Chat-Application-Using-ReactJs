ecb6ceeeb27750284fbcbf4c76a1d9986e3d2424
Ranjeet Kumar Sah <119248522+Ranjeetgithubrit@users.noreply.github.com>
2024-03-27 18:11:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '5cab712c-1cbe-4dd7-8a88-e6440f78dd4c';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+
+    />
+  );
+};
+
+
+
+export default App;
